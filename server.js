21536014debc2eb21280a7fc1b16f75699f56846
Apinut Mutasuwong <120626335+ApinutMutasuwong@users.noreply.github.com>
2023-11-12 14:40:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,27 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('wss');
-const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 
+const server = https.createServer({
+  cert: fs.readFileSync('certificate.crt'),
+  key: fs.readFileSync('private.key')
+});
 
-const app = express();
-app.use(cors());
-const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  
+
   ws.on('message', (message) => {
-    const data = JSON.parse(message);
-    const { latitude, longitude } = data;
-
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ latitude, longitude }));
-
-        // Send a custom event directly to the HTML file
-        client.send(JSON.stringify({ type: 'update-map', data: { latitude, longitude } }));
-      }
-    });
+    console.log(`Received message: ${message}`);
+    // You can process the message here and send a response if needed
+    ws.send('Server received your message');
   });
-  
+
   ws.on('close', () => {
     console.log('Client disconnected');
   });
 });
 
-// Serve your HTML page
-app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/show_pin_location.html');
-});
-
 server.listen(4000, () => {
-  console.log('Server is running on port 4000');
+  console.log('Server listening on port 4000');
 });
