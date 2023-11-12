96e6ec97e522bb2d986a8ca6fd468e268dae0402
Apinut Mutasuwong <120626335+ApinutMutasuwong@users.noreply.github.com>
2023-11-12 18:39:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
-const http = require('http');
+const fs = require('fs');
+const https = require('https');
 const WebSocket = require('ws');
 
-const server = http.createServer();
+const server = https.createServer({
+  cert: fs.readFileSync('../certificate.crt'),
+  key: fs.readFileSync('../private.key')
+});
 
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+  console.log('WebSocket connection established');
 
   ws.on('message', (message) => {
     console.log(`Received message: ${message}`);
-    // You can send a response back to the client if needed
-    // ws.send(`Server received message: ${message}`);
   });
 
   ws.on('close', () => {
-    console.log('Client disconnected');
+    console.log('WebSocket connection closed');
   });
 });
-
-const PORT = 4000;
-
-// Listen for incoming HTTP requests
-server.on('request', (req, res) => {
-  // You can handle HTTP requests here if needed
-});
-
-// Start the server only if it's not already listening
-if (!module.parent) {
-  server.listen(PORT, () => {
-    console.log(`WebSocket server listening on port ${PORT}`);
-  });
-}
