82a76d1854985bbc7a94e6cecb96cfc47d03aaa6
Apinut Mutasuwong <120626335+ApinutMutasuwong@users.noreply.github.com>
2023-11-12 15:51:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const fs = require('fs');
 const https = require('https');
 const WebSocket = require('ws');
-const cors = require('cors');
 
 const server = https.createServer({
   cert: fs.readFileSync('../certificate.crt'),
   key: fs.readFileSync('../private.key')
 });
 
-const wss = new WebSocket.Server({ noServer: true });
-
-// Apply CORS middleware to handle the WebSocket handshake
-server.on('upgrade', (request, socket, head) => {
-  cors()(request, null, () => {
-    wss.handleUpgrade(request, socket, head, (ws) => {
-      wss.emit('connection', ws, request);
-    });
-  });
-});
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('WebSocket connection established');
@@ -34,3 +24,4 @@ wss.on('connection', (ws) => {
 server.listen(4000, () => {
   console.log('Server is listening on port 4000');
 });
+
